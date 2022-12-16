232a6fe93432151013ec924b2f2b6f3d7a105300
TianaNanta <andrianjakananta@gmail.com>
2022-12-16 18:37:00 +0300

    update database

--- app/database.py
+++ app/database.py
@@ -7,10 +7,16 @@ from app.core.config import settings
 engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 @as_declarative()
 class Base:
 
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
