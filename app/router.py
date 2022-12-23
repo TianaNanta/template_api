8b4a2f1f5806b5ce892fce592b963ee79aa583cc
TianaNanta <andrianjakananta@gmail.com>
2022-12-23 17:45:50 +0300

    router replaced

--- app/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .users import crud as userCrud
-from .blogs import crud as blogCrud
-
-root_api_rooter = APIRouter(prefix="/api")
-
-root_api_rooter.include_router(userCrud.router)
-root_api_rooter.include_router(blogCrud.router)
