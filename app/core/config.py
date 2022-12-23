f0eaadc12e984f49dae9e1cab09235f75c5653f1
TianaNanta <andrianjakananta@gmail.com>
2022-12-23 17:46:19 +0300

    configuration updated

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,12 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
+
+    # jwt config
+    JWT_SECRET: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
